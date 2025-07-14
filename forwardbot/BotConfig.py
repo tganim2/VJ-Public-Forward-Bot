1a4bc9f69576bea6ea4ed719bc57150366e50ee5
tganim2 <tganim2@gmail.com>
2025-07-14 12:21:54 +0600

    Update BotConfig.py

--- forwardbot/BotConfig.py
+++ forwardbot/BotConfig.py
@@ -1,10 +1,10 @@
 from os import environ
 class Config(object):
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = environ.get("API_ID", "26649585")
+    API_HASH = environ.get("API_HASH", "588a3ea6fd01ae88bd2e10fed7d55b2c")
     BOT_TOKEN = environ.get("BOT_TOKEN", "")
     STRING_SESSION = environ.get("STRING_SESSION", "")
-    SUDO_USERS = environ.get("SUDO_USERS", "")
+    SUDO_USERS = environ.get("SUDO_USERS", "7822720438 7945551029")
     COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "^/")
     HELP_MSG = """
     💢 **ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅs ɪɴ ᴛʜᴇ ʙᴏᴛ ᴀʀᴇ:**
